82275805973db9dce41a02145c10b0379e4fa86d
Soworksl <Jimy.waner11@gmail.com>
2021-07-21 20:19:37 -0400

    AesEncriptation for string added

---
+++ JWTools.Security.Cryptography/JWTools.Security.Cryptography/AESCriptographer.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace JWTools.Security.Cryptography
+{
+    public static class AESCriptographer
+    {
+        public static string EncryptString(string plainText, string key = "default")
+        {
+            if (string.IsNullOrEmpty(key)) throw new ArgumentNullException("the key cannot be null");
+
+            string output;
+            byte[] processedKey = ProcessStringKey(key);
+            byte[] processedIv = processedKey.Take(16).ToArray();
+
+            using(AesManaged aes = new AesManaged() { Key = processedKey, IV = processedIv })
+            {
+                using(ICryptoTransform encryptor = aes.CreateEncryptor())
+                {
+                    using(MemoryStream memoryStream = new MemoryStream())
+                    {
+                        using( CryptoStream cryptoStream = new CryptoStream(memoryStream, encryptor, CryptoStreamMode.Write))
+                        {
+                            using (StreamWriter streamWriter = new StreamWriter(cryptoStream, Encoding.UTF8))
+                            {
+                                streamWriter.Write(plainText);
+                            }
+                        }
+                        output = Convert.ToBase64String(memoryStream.ToArray());
+                    }
+                }
+            }
+
+            return output;
+        }
+
+        public static string DecryptString(string cipherText, string key = "default")
+        {
+            if (string.IsNullOrEmpty(key)) throw new ArgumentNullException("the key cannot be null");
+
+            string output;
+            byte[] processedKey = ProcessStringKey(key);
+            byte[] processedIv = processedKey.Take(16).ToArray();
+
+            using (AesManaged aes = new AesManaged() { Key = processedKey, IV = processedIv })
+            {
+                using (ICryptoTransform encryptor = aes.CreateDecryptor())
+                {
+                    using (MemoryStream memoryStream = new MemoryStream(Convert.FromBase64String(cipherText)))
+                    {
+                        using (CryptoStream cryptoStream = new CryptoStream(memoryStream, encryptor, CryptoStreamMode.Read))
+                        {
+                            using(StreamReader streamReader = new StreamReader(cryptoStream, Encoding.UTF8))
+                            {
+                                output = streamReader.ReadToEnd();
+                            }
+                        }
+                    }
+                }
+            }
+
+            return output;
+        }
+
+        private static byte[] ProcessStringKey(string key)
+        {
+            byte[] output = new byte[32];
+            byte[] unprocesedKey = Encoding.UTF8.GetBytes(key);
+            using (SHA256 sHA256 = SHA256.Create())
+            {
+                output = sHA256.ComputeHash(unprocesedKey);
+            }
+
+            return output;
+        }
+    }
+}
---
+++ JWTools.Security.Cryptography/Test/AESCryptographerTest.cs
@@ -0,0 +1,57 @@
+﻿using JWTools.Security.Cryptography;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test
+{
+    [TestClass]
+    public class AESCryptographerTest
+    {
+        [DataRow("Hola mundo")]
+        [DataRow("ñiñí que ñoño", DisplayName = "Encrypt And Decrypt String Correctly With spanish characters")]
+        [DataRow("kakarikó!\"@#$%^&*()_-+=`~", DisplayName = "Encrypt And Decrypt String Correctly With Symbol")]
+        [DataRow("\tEsto no me gusta\nAmi si me gusta;\n\tY ami tambien?", DisplayName = "Encrypt And Decrypt String Correctly With escapes")]
+        [TestMethod]
+        public void EncryptAndDecryptStringcorrectly(string testText)
+        {
+            string testKey = "testKey";
+            string encriptedText;
+            string decryptedText;
+
+            encriptedText = AESCriptographer.EncryptString(testText, testKey);
+            decryptedText = AESCriptographer.DecryptString(encriptedText, testKey);
+
+            Assert.AreEqual(testText, decryptedText);
+        }
+
+        [TestMethod]
+        public void EncryptAndDecryptStringCorrectlyWithDefaultKey()
+        {
+            string plainText = "Hello World!";
+            string encriptedText;
+            string decryptedText;
+
+            encriptedText = AESCriptographer.EncryptString(plainText);
+            decryptedText = AESCriptographer.DecryptString(encriptedText);
+
+            Assert.AreEqual(plainText, decryptedText);
+        }
+
+        [ExpectedException(typeof(System.ArgumentNullException))]
+        [TestMethod]
+        public void EncryptString_throwAnExceptionWhenTheKeyIsNullOrEmpty()
+        {
+            string plainText = "Hello World!";
+
+            AESCriptographer.EncryptString(plainText, "");
+        }
+
+        [ExpectedException(typeof(System.ArgumentNullException))]
+        [TestMethod]
+        public void DecryptString_throwAnExceptionWhenTheKeyIsNullOrEmpty()
+        {
+            string plainText = "Hellow";
+
+            AESCriptographer.DecryptString(plainText, null);
+        }
+    }
+}
