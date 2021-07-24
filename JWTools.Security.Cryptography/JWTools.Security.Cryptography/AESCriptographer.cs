29b9e96974098470464243dbb684ac35bcbf91f0
Soworksl <Jimy.waner11@gmail.com>
2021-07-23 20:21:30 -0400

    methods for encrypt and decrypt for files added

--- JWTools.Security.Cryptography/JWTools.Security.Cryptography/AESCriptographer.cs
+++ JWTools.Security.Cryptography/JWTools.Security.Cryptography/AESCriptographer.cs
@@ -3,34 +3,34 @@ using System.IO;
 using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace JWTools.Security.Cryptography
 {
     public static class AESCriptographer
     {
+        public const string ENCRYPTED_FILE_EXTENSION = "jwlock";
+
         public static string EncryptString(string plainText, string key = "default")
         {
-            if (string.IsNullOrEmpty(key)) throw new ArgumentNullException("the key cannot be null");
+
+            if (string.IsNullOrEmpty(plainText)) throw new ArgumentNullException("the plainText to encrypt cannot be null or empty");
+            if (string.IsNullOrEmpty(key)) throw new ArgumentNullException("the key cannot be null or empty");
 
             string output;
-            byte[] processedKey = ProcessStringKey(key);
-            byte[] processedIv = processedKey.Take(16).ToArray();
 
-            using(AesManaged aes = new AesManaged() { Key = processedKey, IV = processedIv })
+            using (ICryptoTransform encryptor = GetCryptoTransformByStringKey(key, KindCryptoTransform.Encryptor))
             {
-                using(ICryptoTransform encryptor = aes.CreateEncryptor())
+                using (MemoryStream memoryStream = new MemoryStream())
                 {
-                    using(MemoryStream memoryStream = new MemoryStream())
+                    using (CryptoStream cryptoStream = new CryptoStream(memoryStream, encryptor, CryptoStreamMode.Write))
                     {
-                        using( CryptoStream cryptoStream = new CryptoStream(memoryStream, encryptor, CryptoStreamMode.Write))
+                        using (StreamWriter streamWriter = new StreamWriter(cryptoStream, Encoding.UTF8))
                         {
-                            using (StreamWriter streamWriter = new StreamWriter(cryptoStream, Encoding.UTF8))
-                            {
-                                streamWriter.Write(plainText);
-                            }
+                            streamWriter.Write(plainText);
                         }
-                        output = Convert.ToBase64String(memoryStream.ToArray());
                     }
+                    output = Convert.ToBase64String(memoryStream.ToArray());
                 }
             }
 
@@ -39,24 +39,20 @@ namespace JWTools.Security.Cryptography
 
         public static string DecryptString(string cipherText, string key = "default")
         {
+            if (string.IsNullOrEmpty(cipherText)) throw new ArgumentNullException("the cipherText to decrypt cannot be null or empty");
             if (string.IsNullOrEmpty(key)) throw new ArgumentNullException("the key cannot be null");
 
             string output;
-            byte[] processedKey = ProcessStringKey(key);
-            byte[] processedIv = processedKey.Take(16).ToArray();
 
-            using (AesManaged aes = new AesManaged() { Key = processedKey, IV = processedIv })
+            using (ICryptoTransform encryptor = GetCryptoTransformByStringKey(key, KindCryptoTransform.Decryptor))
             {
-                using (ICryptoTransform encryptor = aes.CreateDecryptor())
+                using (MemoryStream memoryStream = new MemoryStream(Convert.FromBase64String(cipherText)))
                 {
-                    using (MemoryStream memoryStream = new MemoryStream(Convert.FromBase64String(cipherText)))
+                    using (CryptoStream cryptoStream = new CryptoStream(memoryStream, encryptor, CryptoStreamMode.Read))
                     {
-                        using (CryptoStream cryptoStream = new CryptoStream(memoryStream, encryptor, CryptoStreamMode.Read))
+                        using (StreamReader streamReader = new StreamReader(cryptoStream, Encoding.UTF8))
                         {
-                            using(StreamReader streamReader = new StreamReader(cryptoStream, Encoding.UTF8))
-                            {
-                                output = streamReader.ReadToEnd();
-                            }
+                            output = streamReader.ReadToEnd();
                         }
                     }
                 }
@@ -65,16 +61,82 @@ namespace JWTools.Security.Cryptography
             return output;
         }
 
-        private static byte[] ProcessStringKey(string key)
+        public static void EncryptFile(string plainFilePath, string outputCipherFilePath = "", string key = "default")
         {
-            byte[] output = new byte[32];
-            byte[] unprocesedKey = Encoding.UTF8.GetBytes(key);
-            using (SHA256 sHA256 = SHA256.Create())
+            if (!File.Exists(plainFilePath)) throw new ArgumentException("the file not exist");
+            if (string.IsNullOrEmpty(key)) throw new ArgumentNullException("the key cannot be null or empty");
+
+            if (string.IsNullOrWhiteSpace(outputCipherFilePath))
+                outputCipherFilePath = Path.ChangeExtension(plainFilePath, ENCRYPTED_FILE_EXTENSION);
+            else if (string.IsNullOrEmpty(Path.GetExtension(outputCipherFilePath)))
+                outputCipherFilePath = Path.ChangeExtension(outputCipherFilePath, ENCRYPTED_FILE_EXTENSION);
+
+            using (ICryptoTransform encryptor = GetCryptoTransformByStringKey(key, KindCryptoTransform.Encryptor))
             {
-                output = sHA256.ComputeHash(unprocesedKey);
+                using (FileStream cipherFileOutput = new FileStream(outputCipherFilePath, FileMode.Create, FileAccess.Write))
+                {
+                    using (CryptoStream cryptoStream = new CryptoStream(cipherFileOutput, encryptor, CryptoStreamMode.Write))
+                    {
+                        using(FileStream plainFileStream = new FileStream(plainFilePath, FileMode.Open, FileAccess.Read))
+                        {
+                            plainFileStream.CopyTo(cryptoStream);
+                        }
+                    }
+                }
+            }
+        }
+
+        public static void DecryptFile(string cipherFilePath, string outputPlainFilePath, string key = "default")
+        {
+            if (string.IsNullOrEmpty(key)) throw new ArgumentNullException("the key cannot be null or empty");
+            if (string.IsNullOrWhiteSpace(cipherFilePath)) throw new ArgumentNullException("cipherFilePath cannot be null");
+            if (string.IsNullOrWhiteSpace(outputPlainFilePath)) throw new ArgumentNullException("outputPlainFilePath cannot be null");
+            if (Path.GetFullPath(cipherFilePath) == Path.GetFullPath(outputPlainFilePath)) 
+                throw new ArgumentException("the cipherFilePath and the outputPlainFilePath cannot be equals");
+
+            using(ICryptoTransform cryptoTransform = GetCryptoTransformByStringKey(key, KindCryptoTransform.Decryptor))
+            {
+                using(FileStream outputPlainFileStream = new FileStream(outputPlainFilePath, FileMode.Create, FileAccess.Write))
+                {
+                    using(CryptoStream cryptoStream = new CryptoStream(outputPlainFileStream, cryptoTransform, CryptoStreamMode.Write))
+                    {
+                        using(FileStream cipherFileStream = new FileStream(cipherFilePath, FileMode.Open, FileAccess.Read))
+                        {
+                            cipherFileStream.CopyTo(cryptoStream);
+                        }
+                    }
+                }
+            }
+        }
+
+        private static ICryptoTransform GetCryptoTransformByStringKey(string key, KindCryptoTransform kindCryptoTransform)
+        {
+            ICryptoTransform output;
+
+            byte[] keyAsByteArray = Encoding.UTF8.GetBytes(key);
+            byte[] keyProcessed;
+            byte[] ivProcessed;
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                keyProcessed = sha256.ComputeHash(keyAsByteArray);
+            }
+            ivProcessed = keyProcessed.Take(16).ToArray();
+
+            using (AesManaged aes = new AesManaged() { Key = keyProcessed, IV = ivProcessed })
+            {
+                if (kindCryptoTransform == KindCryptoTransform.Encryptor)
+                    output = aes.CreateEncryptor();
+                else
+                    output = aes.CreateDecryptor();
             }
 
             return output;
+        }
+
+        private enum KindCryptoTransform
+        {
+            Encryptor,
+            Decryptor
         }
     }
 }
--- JWTools.Security.Cryptography/Test/AESCryptographerTest.cs
+++ JWTools.Security.Cryptography/Test/AESCryptographerTest.cs
@@ -1,5 +1,7 @@
 ﻿using JWTools.Security.Cryptography;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
 
 namespace Test
 {
@@ -52,6 +54,52 @@ namespace Test
             string plainText = "Hellow";
 
             AESCriptographer.DecryptString(plainText, null);
+        }
+
+        [ExpectedException(typeof(System.ArgumentNullException))]
+        [DataRow(null, DisplayName = "EncryptString throw an ArgumentNullException when the plainText is null")]
+        [DataRow("", DisplayName = "EncryptString throw an ArgumentNullException when the plaintText is empty")]
+        [TestMethod]
+        public void EncryptString_ThrowAnExceptionWhenThePlainTextIsNullOrEmpty(string plainText)
+        {
+            AESCriptographer.EncryptString(plainText);
+        }
+
+        [ExpectedException(typeof(System.ArgumentNullException))]
+        [DataRow(null, DisplayName = "DecryptString throw an ArgumentNullException when the cipherText is null")]
+        [DataRow("", DisplayName = "DecryptString throw an ArgumentNullException when the CipherText is empty")]
+        [TestMethod]
+        public void DecryptString_ThrownAnExceptionWhenTheCipherTextIsNullOrEmpty(string cipherText)
+        {
+            AESCriptographer.DecryptString(cipherText);
+        }
+
+        [TestMethod]
+        public void EncryptAndDecryptFile()
+        {
+            bool debug = false;
+            string dataTest = "Esto es un archivo de prueba que tiene que ser encriptado";
+            string folderTest = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "EncriptorTest");
+            string testerFilePath = Path.Combine(folderTest, "Encriptenme.txt");
+            string encriptedFilePath = Path.Combine(folderTest, "ArchivoEncriptado." + AESCriptographer.ENCRYPTED_FILE_EXTENSION);
+            string decryptedFilePath = Path.Combine(folderTest, "ArchivoDesencriptado.txt");
+
+            if(Directory.Exists(folderTest))
+                Directory.Delete(folderTest, true);
+
+            Directory.CreateDirectory(folderTest);
+
+            File.WriteAllText(testerFilePath, "Esto es un archivo de prueba que tiene que ser encriptado");
+
+            AESCriptographer.EncryptFile(testerFilePath, encriptedFilePath);
+            AESCriptographer.DecryptFile(encriptedFilePath, decryptedFilePath);
+
+            string result = File.ReadAllText(decryptedFilePath);
+
+            if(!debug)
+                Directory.Delete(folderTest, true);
+
+            Assert.AreEqual(dataTest, result);
         }
     }
 }
